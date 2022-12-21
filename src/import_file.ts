3bff1e426360ea4bb7e925907f38d9bfe2302ddb
mSamiolo <m.samiolo@brambleenergy.com>
2022-12-21 18:35:08 +0000

    first commit

--- src/import_file.ts
+++ src/import_file.ts
@@ -4,22 +4,24 @@ import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 export default function put_in_scope_external_geom_input(scene: THREE.Scene) {
   // Upload and read the document
   document.getElementById("fileInput").addEventListener("change", (event) => {
-    var file = (event.target as HTMLInputElement).files[0];
+    var file_list: FileList = (event.target as HTMLInputElement).files;
 
-    const fileReader = new FileReader();
+    Array.from(file_list).forEach((file) => {
+      const fileReader = new FileReader();
 
-    fileReader.onload = () => {
-      // console.log(fileReader.result);
-      const objLoader = new OBJLoader();
-      const new_group = objLoader.parse(fileReader.result as string);
-      new_group.position.y = 0.8;
-      scene.add(new_group);
-    };
+      fileReader.onload = () => {
+        // console.log(fileReader.result);
+        const objLoader = new OBJLoader();
+        const new_group = objLoader.parse(fileReader.result as string);
+        new_group.position.y = 0.8;
+        scene.add(new_group);
+      };
 
-    fileReader.onerror = () => {
-      console.error(fileReader.error);
-    };
+      fileReader.onerror = () => {
+        console.error(fileReader.error);
+      };
 
-    fileReader.readAsText(file);
+      fileReader.readAsText(file);
+    });
   });
 }
